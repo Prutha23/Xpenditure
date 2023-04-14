b6023562331a2090e5a5ae1402edf4c3fba7f49a
Prutha23 <32551249+Prutha23@users.noreply.github.com>
2023-04-14 19:58:07 -0400

    Update db_connect.py

--- backend/repository/db_connect.py
+++ backend/repository/db_connect.py
@@ -2,4 +2,4 @@ import pyodbc as odbc
 
 
 def get_connection():
-    return odbc.connect('DRIVER={SQL Server};SERVER=Urvil\MSSQLSERVER2K23;DATABASE=Xpenditure;')
+    return odbc.connect('DRIVER={SQL Server};SERVER=Prutha\MSSQLSERVER_P;DATABASE=Xpenditure;')
